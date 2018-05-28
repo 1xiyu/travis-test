238e12830a0e3f2511de0f7bc1622c6898b61b37
litchi <huyongcai@qiyi.com>
2018-05-28 20:25:22 +0800

    ff

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function(config) {
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-			browsers: ['Chrome'],
+		browsers: ['Chrome'],
 
 		// you can define custom flags
 		customLaunchers: {
@@ -70,7 +70,7 @@ module.exports = function(config) {
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
-		singleRun: false,
+		singleRun: true,
 
 		// Concurrency level
 		// how many browser should be started simultaneous
